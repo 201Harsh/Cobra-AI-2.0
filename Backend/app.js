01130c962dcc248908d5e0ab43a499d56fdd3a15
Harsh <gamerpandeyharsh@gmail.com>
2025-10-04 20:01:34 +0530

    added Cors and Cookie Parser

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,14 @@ const app = express();
 
 const ConnectToDB = require("./config/db");
 ConnectToDB();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const USerRouter = require("./routes/user.route");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/users", USerRouter);
 

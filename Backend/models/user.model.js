656289983ee2df9847f69db54dd4fca9e78e54c3
Harsh <gamerpandeyharsh@gmail.com>
2025-10-04 16:28:33 +0530

    Updated UserModel

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -10,10 +10,13 @@ const UserModel = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
+    trim: true,
   },
   role: {
     type: String,

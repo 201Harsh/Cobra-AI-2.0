10c1378de67e471d7e1a1e287b7260e8abcae5db
Harsh <gamerpandeyharsh@gmail.com>
2025-10-04 16:29:44 +0530

    Updated Logic

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -4,7 +4,11 @@ const ValidateMiddleware = require("../middlewares/validate.middleware");
 const { body } = require("express-validator");
 
 router.post("/register", [
-  body("name").notEmpty().withMessage("Name is required"),
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters"),
   body("email").isEmail().withMessage("Email is invalid"),
   body("password")
     .isLength({ min: 6 })

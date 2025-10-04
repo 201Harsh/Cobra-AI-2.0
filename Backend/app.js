9fc09bc78a11431f04896c78d2cdd7af7be06ee7
Harsh <gamerpandeyharsh@gmail.com>
2025-10-04 12:33:59 +0530

    Backend Basic Folder Structure Setup Done

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+const ConnectToDB = require("./config/db");
+ConnectToDB();
+
+const USerRouter = require("./routes/user.route");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", USerRouter);
 
 module.exports = app;

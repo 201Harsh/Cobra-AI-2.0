dd16060f66cefbf02dbf7005b560fe4f507dd530
Harsh <gamerpandeyharsh@gmail.com>
2025-10-07 09:20:29 +0530

    Created Templates Model

---
+++ Backend/models/Templates.model.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+
+const TemplatesSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  programming_language: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  cover_img: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "free",
+    enum: ["free", "premium"],
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+
+  examples_img: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  features: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  tech_stack: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  author: {
+    type: String,
+    required: true,
+  },
+  code_url: {
+    type: String,
+    required: true,
+  },
+  demo_url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  uses: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Templates = mongoose.model("Templates", TemplatesSchema);
+
+module.exports = Templates;

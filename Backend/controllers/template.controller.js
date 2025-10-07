b2740e7a411f9cb9df49dc9546c915dea35128ad
Harsh <gamerpandeyharsh@gmail.com>
2025-10-07 09:50:22 +0530

    update code logic

--- Backend/controllers/template.controller.js
+++ Backend/controllers/template.controller.js
@@ -1,3 +1,4 @@
 const TemplateModel = require('../models/Templates.model');
+const TemplateService = require('../services/template.service');
 
 module.exports.CreateTemplate = async (req, res) => {}

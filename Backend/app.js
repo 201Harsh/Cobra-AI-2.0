557e46363fe55896578c36b8546fffbf9cd62d24
Harsh <gamerpandeyharsh@gmail.com>
2025-10-07 08:50:58 +0530

    updated app sever

--- Backend/app.js
+++ Backend/app.js
@@ -11,12 +11,12 @@ const USerRouter = require("./routes/user.route");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: process.env.CLIENT_URL,
-        credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/users", USerRouter);
 

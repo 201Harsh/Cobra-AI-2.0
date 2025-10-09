3ee610828445fdae85210012fb3e3cca9047f667
Harsh <gamerpandeyharsh@gmail.com>
2025-10-09 11:38:33 +0530

    Web Container Instace Created

--- frontend/app/site/WebContainerInstance.tsx
+++ frontend/app/site/WebContainerInstance.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+// /src/lib/webcontainerInstance.js
+import { WebContainer } from "@webcontainer/api";
 
-const WebContainerInstance = () => {
-  return (
-    <div>WebContainerInstance</div>
-  )
+let webcontainerInstance: any = null;
+
+export async function getWebContainer() {
+  if (webcontainerInstance) return webcontainerInstance;
+
+  console.log("🟢 Booting WebContainer...");
+  webcontainerInstance = await WebContainer.boot();
+  console.log("✅ WebContainer ready");
+
+  return webcontainerInstance;
 }
-
-export default WebContainerInstance

acd1a6c7084ca011c87352a987c87ebf864ee600
Harsh <gamerpandeyharsh@gmail.com>
2025-10-11 13:35:31 +0530

    updated userModel

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -31,7 +31,7 @@ const UserModel = new mongoose.Schema({
   mode: {
     type: String,
     enum: ["creator", "developer"],
-    default: "creator",
+    default: "",
   },
   siteGenToken: {
     type: Number,

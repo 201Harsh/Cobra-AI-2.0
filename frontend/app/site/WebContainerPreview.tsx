3157a779db6582f1f70f493e471ddf917754785f
Harsh <gamerpandeyharsh@gmail.com>
2025-10-11 16:10:22 +0530

    updated Preview Page

--- frontend/app/site/WebContainerPreview.tsx
+++ frontend/app/site/WebContainerPreview.tsx
@@ -3,11 +3,9 @@ import React, { useEffect, useState } from "react";
 import { runReactApp } from "./WebContainerRunner";
 
 export default function WebContainerPreview({ code }: any) {
-  const [logs, setLogs] = useState<any[]>([]);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const handleRun = async () => {
-    setLogs([]);
     await runReactApp(code, (url: string) => setPreviewUrl(url));
   };
 

4967a27252bce306685cb5e6bcb0124ba911401e
Harsh <gamerpandeyharsh@gmail.com>
2025-10-11 16:16:23 +0530

    Updated Logic of WebContainers

--- frontend/app/site/WebContainerPreview.tsx
+++ frontend/app/site/WebContainerPreview.tsx
@@ -1,17 +1,31 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { runReactApp } from "./WebContainerRunner";
+import LoadingScreen from "../auto/LoadingScreen";
 
 export default function WebContainerPreview({ code }: any) {
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [IsLoading, setIsLoading] = useState<boolean>(true);
 
   const handleRun = async () => {
-    await runReactApp(code, (url: string) => setPreviewUrl(url));
+    setIsLoading(true);
+    await runReactApp(code, (url: string) => {
+      setPreviewUrl(url);
+      setIsLoading(false);
+    });
   };
 
   useEffect(() => {
     handleRun();
   }, [code]);
+
+  if (IsLoading) {
+    return (
+      <>
+        <LoadingScreen />
+      </>
+    );
+  }
 
   return (
     <div className="space-y-4 h-screen  w-full">

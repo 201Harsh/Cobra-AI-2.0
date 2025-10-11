9238895cc305752acdd57e6d38fc72a74e0187ca
Harsh <gamerpandeyharsh@gmail.com>
2025-10-11 15:50:28 +0530

    added Code Logic to WebContainers

--- frontend/app/site/[id]/page.tsx
+++ frontend/app/site/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import WebContainerPreview from "../WebContainerPreview";
+import { toast, Zoom } from "react-toastify";
+import AxiosInstance from "@/config/Axios";
+import { useParams } from "next/navigation";
 
 export default function EditorPage() {
   const [code, setCode] = useState(`
@@ -14,6 +17,35 @@ const page = () => {
 
 export default page
 `);
+
+  const params = useParams();
+  const id = params.id;
+
+  const handleGetCode = async () => {
+    try {
+      const res = await AxiosInstance.get(`websites/g/${id}`);
+
+      if (res.status === 200) {
+        console.log(res.data);
+      }
+    } catch (error: any) {
+      toast.error(error.response.data.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Zoom,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleGetCode();
+  }, []);
 
   return (
     <div className="scrollbar-hide min-h-screen">

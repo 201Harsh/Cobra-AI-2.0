deb349b096e02befaf56ea6c3b60352fa3e52427
Harsh <gamerpandeyharsh@gmail.com>
2025-10-13 15:47:32 +0530

    Updated Chat Route and added it

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ const USerRouter = require("./routes/user.route");
 const AIRouter = require("./routes/ai.route");
 const WebsiteRouter = require("./routes/website.route");
 const LabRouter = require("./routes/venomlab.route");
+const ChatRouter = require("./routes/chat.route");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,5 +26,6 @@ app.use("/users", USerRouter);
 app.use("/ai", AIRouter);
 app.use("/websites", WebsiteRouter);
 app.use("/lab", LabRouter);
+app.use("/chat", ChatRouter);
 
 module.exports = app;
--- Backend/routes/chat.route.js
+++ Backend/routes/chat.route.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const ChatContaller = require("../controllers/chat.controller");
 const AuthMiddleware = require("../middlewares/auth.middleware");
 
-router.post("/get/all", AuthMiddleware.AuthUser, ChatContaller.SaveChats);
+router.get("/get/all", AuthMiddleware.AuthUser, ChatContaller.SaveChats);
 
 module.exports = router;

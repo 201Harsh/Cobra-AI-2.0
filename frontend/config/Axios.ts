cfd7dc0e521adfc126de8c2aa6070e743a951558
Harsh <gamerpandeyharsh@gmail.com>
2025-10-15 15:23:29 +0530

    updated COde

--- frontend/config/Axios.ts
+++ frontend/config/Axios.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
+const token = localStorage.getItem("token");
+
 
 const AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true, // ✅ keeps cookie attached in requests
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export default AxiosInstance;

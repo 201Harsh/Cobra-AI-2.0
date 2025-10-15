f98272f01a8f980fa181a9d5af813c01dda6c895
Harsh <gamerpandeyharsh@gmail.com>
2025-10-15 09:13:40 +0530

    added Rate Limit in Venom Lab

--- Backend/routes/venomlab.route.js
+++ Backend/routes/venomlab.route.js
@@ -3,23 +3,34 @@ const VenomLabController = require("../controllers/venomlab.controller");
 const AuthMiddleware = require("../middlewares/auth.middleware");
 const ValidateMiddleware = require("../middlewares/validate.middleware");
 const { body } = require("express-validator");
+const RateLimitMiddleware = require("../middlewares/rate-limit.middleware");
 
 router.post(
   "/create",
   [body("name").notEmpty().withMessage("Name is required")],
   [body("environment").notEmpty().withMessage("Environment is required")],
   AuthMiddleware.AuthUser,
-  VenomLabController.createVenomLab,
-  ValidateMiddleware.validateUser
+  ValidateMiddleware.validateUser,
+  RateLimitMiddleware.venomLabLimit,
+  VenomLabController.createVenomLab
 );
 
-router.get("/all", VenomLabController.getVenomLabs);
+router.get(
+  "/all",
+  RateLimitMiddleware.GlobalLimit,
+  VenomLabController.getVenomLabs
+);
 
-router.get("/one/:id", VenomLabController.getVenomLabById);
+router.get(
+  "/one/:id",
+  RateLimitMiddleware.GlobalLimit,
+  VenomLabController.getVenomLabById
+);
 
 router.delete(
   "/delete/:id",
   AuthMiddleware.AuthUser,
+  RateLimitMiddleware.GlobalLimit,
   VenomLabController.deleteVenomLab
 );
 

776b63de121a886e98434bf61ac32245cbebf062
Harsh <gamerpandeyharsh@gmail.com>
2025-10-15 09:07:33 +0530

    Added Rate Limit In Chat Route

--- Backend/routes/chat.route.js
+++ Backend/routes/chat.route.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
 const ChatContaller = require("../controllers/chat.controller");
 const AuthMiddleware = require("../middlewares/auth.middleware");
+const RateLimitMiddleware = require("../middlewares/rate-limit.middleware");
 
-router.get("/get/all", AuthMiddleware.AuthUser, ChatContaller.getAllChats);
+router.get(
+  "/get/all",
+  AuthMiddleware.AuthUser,
+  RateLimitMiddleware.GlobalLimit,
+  ChatContaller.getAllChats
+);
 
-router.delete("/del/all", AuthMiddleware.AuthUser, ChatContaller.DelAllChats);
+router.delete(
+  "/del/all",
+  AuthMiddleware.AuthUser,
+  RateLimitMiddleware.GlobalLimit,
+  ChatContaller.DelAllChats
+);
 
 module.exports = router;

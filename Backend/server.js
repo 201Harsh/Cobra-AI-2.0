132eaa3f60af0765ccb7508a207593b5b04dba20
Harsh <gamerpandeyharsh@gmail.com>
2025-10-15 14:33:00 +0530

    Production Done

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;

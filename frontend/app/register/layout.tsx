44d55d115981b91533359b3e8ed9d89725b4ac24
Harsh <gamerpandeyharsh@gmail.com>
2025-10-15 21:42:38 +0530

    updated Auto Redirect logic

--- frontend/app/register/layout.tsx
+++ frontend/app/register/layout.tsx
@@ -1,24 +1,7 @@
 // app/creator/layout.tsx
 import "../globals.css";
 import React from "react";
-import type { Metadata } from "next";
 import AutoRedirect from "../Custom/AutoRedirect";
-
-const BaseUrl = process.env.SEO_URL || "http://localhost:3000";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(BaseUrl),
-  title: "Cobra AI - Creator Dashboard",
-  description:
-    "Cobra AI 2.0 Creator Dashboard — Build, manage, and deploy AI-powered websites effortlessly.",
-  authors: [
-    { name: "Harsh Pandey", url: "https://www.instagram.com/201harshs/" },
-  ],
-  icons: {
-    icon: "/img/logo.png",
-    shortcut: "/img/logo.png",
-  },
-};
 
 export default function CreatorLayout({
   children,
